4991c8ac5477940930ccf66c76498d9a27e1dbb5
TzeBohn Ling <tzebohn@TzeBohns-MacBook-Pro.local>
2026-06-25 11:45:43 -0400

    refactor: Move FAQ section up and add new questions

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -473,6 +473,78 @@ export default function Home () {
                     </div>
                 </section>
 
+                {/* FAQ Section */}
+                <section className="relative min-h-screen flex items-center justify-center">
+                    {/* Background image */}
+                    <div
+                        className="absolute inset-0 bg-cover bg-center"
+                        style={{ backgroundImage: `url(${faqBackground})` }}
+                    />
+
+                    {/* Grayed out overlay */}
+                    <div className="absolute inset-0 bg-black/50"/>
+
+                    <div className="relative flex justify-center items-center max-w-7xl mx-auto py-20">
+                        <div className="">
+                            <h1 
+                                className="
+                                    text-white/90 text-xl xs:text-4xl sm:text-5xl 
+                                    tracking-[0.3em] font-[Orbitron]
+                                    text-center uppercase font-bold 
+                                    transition-all duration-300
+                                    drop-shadow-[0_0_8px_rgba(37,150,190,0.25)]
+                                    glow-text py-10 mb-10
+                                " 
+                                >
+                                    Frequently Asked Questions
+                            </h1>
+
+                            {/* Questions section */}
+                            <div className="flex flex-col gap-8 p-4 md:max-w-5xl md:mx-auto">
+                                <FaqCards
+                                    title={"How do you pronounce your name?"}
+                                    description={"I know my name can be a little tricky at first glance — it's pronounced [Tea-zz Bohn]. Like John but with a B."} 
+                                />
+                                <FaqCards
+                                    title={"Who are you and what do you do?"}
+                                    description={`
+                                        I am a self-taught Full Stack Web Dev. I primarily work with React and Node.js,
+                                        and I've been spending a lot of time building applications after graduating college.
+                                        `
+                                    }
+                                />
+                                <FaqCards
+                                    title={"What technologies do you work with?"}
+                                    description={`
+                                        React, TailwindCSS, Javascript, Typescript, HTML & CSS, TanStackQuery, Firebase, 
+                                        Node.js, Express, Axios, Prisma, Redis, SQL, AWS, Render, Git, etc.
+                                        `
+                                    } 
+                                />
+                                <FaqCards
+                                    title={"What makes you different from others in your field?"}
+                                    description={`
+                                        I'd say one of my strongest traits is how adaptable and motivated I am. In the fast 
+                                        evolving field of software engineering with AI, staying flexible and continuously
+                                        learning is crucial. Therefore, I study to be a generalist, where I focus on the underlying
+                                        concepts behind commonly used tools, rather than just syntax (which can easily be done by AI). 
+                                        I find it more important to know what tools are available and when to apply them.
+                                        `
+                                    }
+                                />
+                                <FaqCards
+                                    title={"Do you have any experience with AI"}
+                                    description={`
+                                        Yes, I've been enjoying the use of TUIs like OpenCode and Codex. With OpenCode I am
+                                        able to experiment with lots of different models including lightweight local LLMs like Gemma 4.
+                                        `
+                                    } 
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
                 {/* Roadmap / Timeline Section*/}
                 <section ref={lineScrollRef} id="roadmap" className="bg-[#03050C] py-10">
                     {/* Heading */}
@@ -583,86 +655,6 @@ export default function Home () {
                                     "Successfully landed a job and achieved employed status 🙂", 
                                 ]}
                             />
-                        </div>
-                    </div>
-                </section>
-
-                {/* FAQ Section */}
-                <section className="relative min-h-screen flex items-center justify-center">
-                    {/* Background image */}
-                    <div
-                        className="absolute inset-0 bg-cover bg-center"
-                        style={{ backgroundImage: `url(${faqBackground})` }}
-                    />
-
-                    {/* Grayed out overlay */}
-                    <div className="absolute inset-0 bg-black/50"/>
-
-                    <div className="relative flex justify-center items-center max-w-7xl mx-auto py-20">
-                        <div className="">
-                            <h1 
-                                className="
-                                    text-white/90 text-xl xs:text-4xl sm:text-5xl 
-                                    tracking-[0.3em] font-[Orbitron]
-                                    text-center uppercase font-bold 
-                                    transition-all duration-300
-                                    drop-shadow-[0_0_8px_rgba(37,150,190,0.25)]
-                                    glow-text py-10 mb-10
-                                " 
-                                >
-                                    Frequently Asked Questions
-                            </h1>
-
-                            {/* Questions section */}
-                            <div className="flex flex-col gap-8 p-4 md:max-w-5xl md:mx-auto">
-                                <FaqCards
-                                    title={"Who are you and what do you do?"}
-                                    description={`
-                                        I'm an aspiring software engineer with a passion for building clean, efficient,
-                                        and scalable solutions. I specialize in full-stack development, where I get to
-                                        work on both the frontend and backend logic. I'm always experimenting with new 
-                                        tools and tech to keep improving and stay on top of what's trending in the industry.
-                                        `
-                                    }
-                                />
-                                <FaqCards
-                                    title={"What makes you different from others in your field?"}
-                                    description={`
-                                        I'd say one of my strongest traits is how adaptable I am. In the fast 
-                                        evolving field of software engineering, staying flexible and continuously
-                                        learning is crucial. Whenever new technologies or tools emerge, I make it
-                                        a priority to understand the concepts behind them. I dive deep into exploring
-                                        how they work, how they can be applied, and how they can improve the way
-                                        I approach problems.
-                                        `
-                                    }
-                                />                         
-                                <FaqCards
-                                    title={"How do you pronounce your name?"}
-                                    description={"I know my name can be a little tricky at first glance — it's pronounced [Tea-zz Bohn]. Like John but with a B."} 
-                                />
-                                <FaqCards
-                                    title={"Where are you based?"}
-                                    description={`
-                                        My parents are from Hong Kong, but I was born and raised in New York, 
-                                        so I’m bilingual and fluent in both English and Cantonese. It's been a huge 
-                                        advantage when it comes to understanding different perspectives and 
-                                        communicating across cultures.
-                                        `
-                                    } 
-                                />
-                                <FaqCards
-                                    title={"What's your background in software engineering?"}
-                                    description={`
-                                        While I don’t have formal job experience yet, I’ve been developing my 
-                                        skills through hands-on projects, both on my own and in collaboration with other
-                                        students. As a self-taught Full Stack Web Developer, I’m always building, 
-                                        experimenting, and pushing my knowledge further. I’m excited to bring that 
-                                        learning into a professional environment.
-                                        `
-                                    }
-                                />
-                            </div>
                         </div>
                     </div>
                 </section>
