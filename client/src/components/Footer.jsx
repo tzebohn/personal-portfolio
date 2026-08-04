748972fc5fb628f43134f2bb84673fc7030244e8
TzeBohn Ling <tzebohn@TzeBohns-MacBook-Pro.local>
2026-08-04 11:27:08 -0400

    ui: improve footer transition

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -46,11 +46,11 @@ export default function Footer () {
     return (
         <>  
             {/* Extra spacing for blending background */}
-           <div className="h-48 bg-gradient-to-b from-[#090e1a] via-[#05070a] to-black"></div>
-
-            <footer className="relative bg-black pt-24 px-20 border-t-2 border-gray-900">
+            <div className="h-32 bg-gradient-to-b from-[#03050C] to-[#0a0d14]"></div>
+            
+            <footer className="relative bg-[#03050C] pt-16 md:pt-24 px-6 md:px-12 lg:px-20 border-t border-gray-900/50">
                 {/* Upper feature panel */}
-                <section className="relative mx-auto max-w-4xl -mt-46 mb-16 text-white">
+                <section className="relative mx-auto max-w-3xl mb-16 text-white">
                     <div 
                         className="
                             flex flex-col gap-4 items-center p-8
@@ -63,7 +63,7 @@ export default function Footer () {
                         "
                     >
                         <h2 className="text-center text-2xl font-semibold tracking-wide">Thanks for visiting</h2>
-                        <p className="text-center text-sm md:text-base tracking-wide">If you're interested in collaborating,
+                        <p className="text-center text-sm md:text-base tracking-wide max-w-md">If you're interested in collaborating,
                             let's schedule a conversation. I'll
                             bring the coffee.
                         </p>
@@ -72,10 +72,10 @@ export default function Footer () {
                             target="_blank"
                             rel="noopener noreferrer"
                             className={`
-                                flex items-center justify-center gap-4 
-                                border rounded-full p-2 xs:px-4
-                                hover:bg-gray-800
-                                active:bg-gray-800
+                                 flex items-center justify-center gap-4 
+                                 border rounded-full p-2 xs:px-4
+                                 hover:bg-gray-800
+                                 active:bg-gray-800
                             `}
                             aria-label="Fill out contact form"
                         >
@@ -85,118 +85,118 @@ export default function Footer () {
                     </div>
                 </section>
 
-                {/* Bottom meta footer */}
-                <div className="relative overflow-hidden w-full">
-                    {/* Atmosphere background layer */}
-                    <div
-                        className="
-                            absolute inset-0 z-0
-                        "
-                    />
+                 {/* Bottom meta footer */}
+                 <div className="relative overflow-hidden w-full">
+                     {/* Atmosphere background layer */}
+                     <div
+                         className="
+                             absolute inset-0 z-0
+                         "
+                     />
+                     
+                     {/* Social buttons */}
+                     <div id="contact" className="relative z-10 flex justify-center pt-24 pb-6">
+                         <div className="grid grid-cols-3 gap-8 md:gap-12">
+                             {/* Github link */}
+                             <a
+                                 href="https://github.com/tzebohn"
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 className={`${anchorStyles}`}
+                                 aria-label="View my Github"
+                             >
+                                 <GithubIcon className={`${iconStyles}`}/>
+                             </a>
+                             
+                             {/* Linkedin link */}
+                             <a
+                                 href="https://www.linkedin.com/in/tzebohn-ling-100a992b1/"
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 className={`${anchorStyles}`}
+                                 aria-label="Connect with me on Linkedin"
+                             >
+                                 <LinkedinIcon className={`${iconStyles}`}/>
+                             </a>
+                             
+                             {/* Discord link */}
+                             <a
+                                 href="https://discord.com/users/922332604622077964"
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 className={`${anchorStyles}`}
+                                 aria-label="Message me on Discord"
+                             >
+                                 <DiscordIcon className={`${iconStyles}`}/>
+                             </a>
+                             
+                             {/* Email link */}
+                             <button
+                                 onClick={handleCopy}
+                                 className={`${anchorStyles} relative`}
+                                 aria-label="Copy email to clipboard"
+                             >
+                                 <EmailIcon className={`${iconStyles}`}/>
+                                 {copied && (
+                                     <span className="absolute -top-6 text-xs bg-black text-white px-2 py-1 rounded">
+                                     Copied!
+                                     </span>
+                                 )}
+                             </button>
+                             
+                             {/* Twitter link */}
+                             <a
+                                 href="https://x.com/tzebohn"
+                                 target="_blank"
+                                 rel="noopener noreferrer"
+                                 className={`${anchorStyles}`}
+                                 aria-label="Follow me on X"
+                             >
+                                 <TwitterIcon className={`${iconStyles}`}/>
+                             </a>
+                             
+                             {/* Instagram link */}
+                             <button
+                                 type="button"
+                                 className={`${anchorStyles}`}
+                                 aria-label="Follow my Instagram"
+                             >
+                                 <InstaIcon className={`${iconStyles}`}/>
+                             </button>
+                         </div>
+                     </div>
+                 </div>
+                 
+                 {/* Divider */}
+                 <div className="mt-10 pb-2 border-t border-gray-900"></div>
+             </footer>
+             
+             <div className="text-white bg-gradient-to-b from-black via-[#04060d] to-[#090e1a]">
+                 <div className="min-h-24 flex flex-col items-center md:flex-row md:justify-between md:items-end px-4">
+                     <nav className="flex flex-wrap justify-center gap-8 order-1 md:order-2">
+                         <a 
+                             href="https://github.com/tzebohn" 
+                             target="_blank" 
+                             rel="noreferrer" 
+                             className="hover:text-[#ED3F0A] focus:text-[#ED3F0A] transition-colors duration-300 uppercase py-3 text-xs"
+                         >
+                             Github
+                         </a>
+                         <button className={`${buttonStyles}`}>Blog</button>
+                         <button className={`${buttonStyles}`}>Terms of Use</button>
+                         <button className={`${buttonStyles}`}>Privacy Policy</button>
+                         <button className={`${buttonStyles}`}>Cookie Policy</button>
+                     </nav>
+                     
+                     <div className="p-2 flex items-end order-2 md:order-1">
+                         <h6 className="flex gap-1 items-center text-sm tracking-wide">
+                             <span>©</span>
+                             2026 TzeBohn Ling. All rights reserved.
+                         </h6>
+                     </div>
+                 </div>
+             </div>
+         </>
+     )
+ }
 
-                    {/* Social buttons */}
-                    <div id="contact" className="relative z-10 flex justify-center pt-12">
-                        <div className="grid grid-cols-3 gap-6">
-                            {/* Github link */}
-                            <a
-                                href="https://github.com/tzebohn"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={`${anchorStyles}`}
-                                aria-label="View my Github"
-                            >
-                                <GithubIcon className={`${iconStyles}`}/>
-                            </a>
-
-                            {/* Linkedin link */}
-                            <a
-                                href="https://www.linkedin.com/in/tzebohn-ling-100a992b1/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={`${anchorStyles}`}
-                                aria-label="Connect with me on Linkedin"
-                            >
-                                <LinkedinIcon className={`${iconStyles}`}/>
-                            </a>
-
-                            {/* Discord link */}
-                            <a
-                                href="https://discord.com/users/922332604622077964"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={`${anchorStyles}`}
-                                aria-label="Message me on Discord"
-                            >
-                                <DiscordIcon className={`${iconStyles}`}/>
-                            </a>
-
-                            {/* Email link */}
-                            <button
-                                onClick={handleCopy}
-                                className={`${anchorStyles} relative`}
-                                aria-label="Copy email to clipboard"
-                            >
-                                <EmailIcon className={`${iconStyles}`}/>
-                                {copied && (
-                                    <span className="absolute -top-6 text-xs bg-black text-white px-2 py-1 rounded">
-                                    Copied!
-                                    </span>
-                                )}
-                            </button>
-
-
-                            {/* Twitter link */}
-                            <a
-                                href="https://x.com/tzebohn"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={`${anchorStyles}`}
-                                aria-label="Follow me on X"
-                            >
-                                <TwitterIcon className={`${iconStyles}`}/>
-                            </a>
-
-                            {/* Instagram link */}
-                            <button
-                                type="button"
-                                className={`${anchorStyles}`}
-                                aria-label="Follow my Instagram"
-                            >
-                                <InstaIcon className={`${iconStyles}`}/>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                {/* Divider */}
-                <div className="mt-10 pb-2 border-t border-gray-900"></div>
-            </footer>
-
-            <div className="text-white bg-gradient-to-b from-black via-[#04060d] to-[#090e1a]">
-                <div className="min-h-24 flex flex-col items-center md:flex-row md:justify-between md:items-end px-4">
-                    <nav className="flex flex-wrap justify-center gap-8 order-1 md:order-2">
-                        <a 
-                            href="https://github.com/tzebohn" 
-                            target="_blank" 
-                            rel="noreferrer" 
-                            className="hover:text-[#ED3F0A] focus:text-[#ED3F0A] transition-colors duration-300 uppercase py-3 text-xs"
-                        >
-                            Github
-                        </a>
-                        <button className={`${buttonStyles}`}>Blog</button>
-                        <button className={`${buttonStyles}`}>Terms of Use</button>
-                        <button className={`${buttonStyles}`}>Privacy Policy</button>
-                        <button className={`${buttonStyles}`}>Cookie Policy</button>
-                    </nav>
-
-                    <div className="p-2 flex items-end order-2 md:order-1">
-                        <h6 className="flex gap-1 items-center text-sm tracking-wide">
-                            <span>©</span>
-                            2026 TzeBohn Ling. All rights reserved.
-                        </h6>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
